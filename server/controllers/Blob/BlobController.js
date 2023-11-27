7840a66d3b5ff94798978dc8f39aca5ea74e3226
totskaianastia <151627514+totskaianastia@users.noreply.github.com>
2023-11-27 21:58:52 +0100

    Added BlobController & AzureBlobService

--- server/controllers/Blob/BlobController.js
+++ server/controllers/Blob/BlobController.js
@@ -1,1 +1,17 @@
-// query handling 
+import * as AzureBlobService from '../../Services/Blob/AzureBlobService.js';
+import * as AzureContainerService from '../../Services/Blob/AzureContainerService.js';
+
+export const create = async (req, res) => {
+    const uploadedFiles = req.files;
+
+    const urls = [];
+    try {
+        for (let i = 0; i < uploadedFiles.length; i++) {
+            const url = await AzureBlobService.upload(uploadedFiles[i].buffer, uploadedFiles[i].size, uploadedFiles[i].originalname);
+            urls.push({url});
+        }
+        res.status(200).json(urls);
+    } catch (error) {
+        console.log(error);
+    }
+}
--- server/router.js
+++ server/router.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import multer from 'multer';
 
 import * as EntityController from './controllers/Table/EntityController.js';
+import * as BlobController from './controllers/Blob/BlobController.js';
 
 const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 
 router.post('/:tableName/create/', EntityController.createEntity);
 router.patch('/:tableName/edit/', EntityController.editEntity);
 router.delete('/:tableName/delete/', EntityController.deleteEntity);
+
+
+router.post('/upload', upload.any(), BlobController.create);
 
 
 export default router;
